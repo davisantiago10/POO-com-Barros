b3b13d97e6016fbe16ce29724e08605694af5583
Davi Santiago Moura <145844746+davisantiago10@users.noreply.github.com>
2025-05-15 18:09:33 -0300

    Add files via upload

---
+++ questão6/PrincipalCalendario.java
@@ -0,0 +1,9 @@
+public class PrincipalCalendario {
+    public static void main(String[] args){
+        Calendario c = new Calendario(20, 11, 2007);
+        Calendario c2 = new Calendario(14, 8, 2008);
+        c.Imprimir();
+        c2.Imprimir();
+    }
+
+}
